d68ca5637141e524d0333636dae9742a94faff5f
Guljigit07 <guljigitaskerov0707@gmail.com>
2024-06-24 19:42:44 +0600

    firist commit

---
+++ src/UI/UIbutton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UIbutton = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default UIbutton
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+
+const rootRedusers = createSlice({
+    
+})
+
+
+export const store = configureStore({reducer: rootRedusers})
